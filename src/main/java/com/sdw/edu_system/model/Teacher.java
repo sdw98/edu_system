c37c4086753e3baf552a602f9003b2e517a3fc3a
sdw98 <sdw9851@gmail.com>
2025-07-04 13:53:00 +0900

    1

---
+++ src/main/java/com/sdw/edu_system/model/Teacher.java
@@ -0,0 +1,15 @@
+package com.sdw.edu_system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Teacher {
+    private Integer id;
+    private String name;
+}
---
+++ src/test/java/com/sdw/edu_system/EduSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdw.edu_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
