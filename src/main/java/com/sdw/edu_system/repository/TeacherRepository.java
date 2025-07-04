7f83869fe0ddc2d9308a98b4a2f18596b56d000b
sdw98 <sdw9851@gmail.com>
2025-07-04 14:43:30 +0900

    5

--- src/main/java/com/sdw/edu_system/repository/TeacherRepository.java
+++ src/main/java/com/sdw/edu_system/repository/TeacherRepository.java
@@ -3,12 +3,26 @@ package com.sdw.edu_system.repository;
 import com.sdw.edu_system.model.Teacher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class TeacherRepository {
     private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Teacher> mapper = (resultSet, rowNum) ->
+            Teacher.builder()
+                    .id(resultSet.getInt("id"))
+                    .name(resultSet.getString("name"))
+                    .build();
+
+    public List<Teacher> findAll() {
+        return jdbcTemplate.query("SELECT * FROM teacher ORDER BY name", mapper);
+    }
+
 
     public int save(Teacher teacher) {
         return jdbcTemplate.update(
