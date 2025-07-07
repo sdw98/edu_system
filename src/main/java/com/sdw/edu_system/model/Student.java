ced12cf4afdec01084c635e53446fa41636685c5
sdw98 <sdw9851@gmail.com>
2025-07-07 13:19:35 +0900

    Student

---
+++ src/main/java/com/sdw/edu_system/model/Student.java
@@ -0,0 +1,19 @@
+package com.sdw.edu_system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    private Integer id;
+    private String name;
+
+    private Integer score;
+    private Integer teacherId;
+    private String teacherName;
+}
